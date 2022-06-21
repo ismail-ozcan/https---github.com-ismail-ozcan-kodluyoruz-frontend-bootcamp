0b16ef8fe981bdab76483bc16476457adc9b9eaa
ISMAIL OZCAN <avukatismailozcan@gmail.com>
2022-06-21 18:05:37 +0300

    first-task

--- first-task/make-group.js
+++ first-task/make-group.js
@@ -1,11 +1,11 @@
 function makeGroup(data){       
   
     let aliceBlueMembers=data.filter(x=>x.group=="AliceBlue");    
-    console.log(aliceBlueMembers);
+    console.log(aliceBlueMembers);   
     let  deepPinkMembers=data.filter(x=>x.group=="DeepPink");
     console.log(deepPinkMembers);
     let orangeRedMembers=data.filter(x=>x.group=="OrangeRed");
-    console.log(orangeRedMembers);
+    console.log(orangeRedMembers);      // bu noktaya kadar filter metodunu kullanarak verileri tasnif ettik ve console'a yazdırdık. console'da görünecek olan şey 3 adet dizidir
     
     
     function showAssistant(par) { 
@@ -13,9 +13,9 @@ function makeGroup(data){
         for(let i=0; i<par.length; i++){                
             if(par[i].assistant){        
              assistantName=par[i].name;
-            }           
-        }
-       console.log(assistantName)
+            }                 
+        }                   // bu fonksiyonun işlevi dizi elemanlarını tek tek gezerek "value" kısmı true olan key"i bulmak ve aynı eleman içerisindeki name bilgisini bize sunmaktır
+       console.log(assistantName)      
     }
 
      showAssistant(aliceBlueMembers);
